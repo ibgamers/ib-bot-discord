cd0120a71bb1a43da6e428315f7862ccdbb49e4c
ibgamers <37729108+ibgamers@users.noreply.github.com>
2018-03-24 03:38:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+
+client.on("ready", () => {
+   console.log("Estoy listo!");
+});
+
+client.on("message", (message) => {
+
+	const args = message.content.trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+  
+	switch(command){
+
+	  	case "automix":
+	  		message.channel.send('* IP [MIX]: 190.0.163.155:27018');
+	  		break;
+	  	case "publico":
+	  		message.channel.send('* IP [PUB]: 190.0.163.155:27019');
+	  		break;
+	  	case "mix":
+	  		message.channel.send('* IP [MIX]: 190.0.163.155:27018');
+	  		break;
+	  	case "pub":
+	  		message.channel.send('* IP [PUB]: 190.0.163.155:27019');
+	  		break;
+	} 
+});
+
+client.login("NDI2MjU1NDAxNTUwNDc5MzYw.DZVV4A.7MdHGgshuHPNPz5FdZQeob5Pkh4");
